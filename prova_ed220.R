d91c4a32763339e929cf775bd5ea397913c37dbe
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-06-21 09:51:48 -0300

    composite reliabilty?

--- prova_ed220.R
+++ prova_ed220.R
@@ -1,9 +1,6 @@
 library(readr)
 Prova19_268 <- read_delim("Prova19_268.csv", ";", escape_double = FALSE, trim_ws = TRUE)
 
-View(Prova19_268)
-
-
 library(lavaan)
 library(semTools)
 
@@ -53,6 +50,16 @@ rec <- '  TOL_INCERT ~ CONTR_EMOC + DESEJO_ENVOLV + FLEXI_COGNITIVA + AUTO_CONTR
 best_ave_model <- paste(best_ave_de, best_ave_fg, best_ave_ac, best_ave_ce, best_ave_ti, rec, sep='\n')
 fit <- sem(best_ave_model, data=Prova19_268)
 reliability(fit)
+
+
+# -- step 1: composite reliability --
+# following recommendations from https://stats.stackexchange.com/questions/97013/calculating-average-variance-extracted-ave-in-r-for-checking-discriminant-vali
+# and https://groups.google.com/forum/#!topic/lavaan/TpWINJo_CRI, it seems that the reliability function's omega3 is a 
+# conservative and therefore appropriate measure of composite reliability. omega 1 and 2 have
+# some simplifying assumptions which i don't understand, so i'll leave them out for now.
+reliability(fit)
+# omega3     0.5836462       0.7058524    0.7961523  0.7136993  0.6249287 0.7583164
+
 
 #summary(fit, standardized=TRUE)
 #semPaths(fit, what="paths", whatLabels="stand", rotation=2)
