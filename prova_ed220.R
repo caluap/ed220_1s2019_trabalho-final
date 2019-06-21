5ed872276690bbe17ed552fab28e53786c0239d9
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2019-06-20 23:37:35 -0300

    attempt at maximizing ave...
    i did this by trial and error, but only one of the
    constructs actually managed to be > 0.5. since
    this is an exercise, i'm probably doing something wrong, but i'll leave it as is for now.

--- prova_ed220.R
+++ prova_ed220.R
@@ -5,7 +5,9 @@ View(Prova19_268)
 
 
 library(lavaan)
+library(semTools)
 
+# model as presented
 ed220Model <- '
   # measurement model
   DESEJO_ENVOLV =~ DE_1 + DE_2 + DE_3 + DE_4 + DE_5 + DE_6 + DE_7 + DE_8
@@ -16,3 +18,43 @@ ed220Model <- '
   # regressions
   TOL_INCERT ~ CONTR_EMOC + DESEJO_ENVOLV + FLEXI_COGNITIVA + AUTO_CONTROL
 '
+
+fit <- sem(ed220Model, data=Prova19_268)
+reliability(fit)
+
+
+# attempt to maximize AVE:
+best_ave_de <- 'DESEJO_ENVOLV =~ DE_1 + DE_2 + DE_6'
+fit <- sem(best_ave_de, data=Prova19_268)
+reliability(fit)
+
+best_ave_fg <- '
+  #FLEXI_COGNITIVA =~ FC_1 + FC_10 + FC_11  # this is ave > .5, but gives out a warning about negative variance
+  FLEXI_COGNITIVA =~ FC_2 + FC_6 + FC_7 + FC_8
+'
+fit <- sem(best_ave_fg, data=Prova19_268)
+reliability(fit)
+
+best_ave_ac <- 'AUTO_CONTROL =~ AC_6 + AC_7 + AC_8'
+fit <- sem(best_ave_ac, data=Prova19_268)
+reliability(fit)
+
+best_ave_ce <- 'CONTR_EMOC =~ Cem_1 + Cem_3 + Cem_4'
+fit <- sem(best_ave_ce, data=Prova19_268)
+reliability(fit)
+
+best_ave_ti <- 'TOL_INCERT =~ TI_1 + TI_4 + TI_5'
+fit <- sem(best_ave_ti, data=Prova19_268)
+reliability(fit)
+
+# not great, not terrible
+
+rec <- '  TOL_INCERT ~ CONTR_EMOC + DESEJO_ENVOLV + FLEXI_COGNITIVA + AUTO_CONTROL'
+best_ave_model <- paste(best_ave_de, best_ave_fg, best_ave_ac, best_ave_ce, best_ave_ti, rec, sep='\n')
+fit <- sem(best_ave_model, data=Prova19_268)
+reliability(fit)
+
+#summary(fit, standardized=TRUE)
+#semPaths(fit, what="paths", whatLabels="stand", rotation=2)
+
+
